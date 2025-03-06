a816bf92854dd179a65b2d98d455d6a1cf72ec11
yulpeng <yulong.peng1@student.unimelb.edu.au>
2025-03-06 11:16:58 +1100

    minus 10 and 2 sdefrgth

--- app/src/main/java/example/Main.java
+++ app/src/main/java/example/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         System.out.println("add = " + calculator.add(3, 6));
-        System.out.println("minus = " + calculator.minus(8, 2));
+        System.out.println("minus = " + calculator.minus(10, 2));
         System.out.println("multiply = " + calculator.multiply(3, 5));
         System.out.println("divide = " + calculator.divide(6, 3));
 
-    }
+    } //sdfghj rftgyhj
 }
